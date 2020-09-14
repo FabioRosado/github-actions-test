3b82f0f16d250120ae6a75dd46e13ae0e23825d5
FabioRosado <fabioglrosado@gmail.com>
2020-09-14 15:14:11 +0100

    Update broken python

--- broken_python.py
+++ broken_python.py
@@ -3,4 +3,11 @@ import os
 
 def test_broken_lint(test):
     """testing broken lint"""
-    print(hello)
+    print('hello')  
+
+def another_test():
+    """Testing things"""
+    print("world") 
+
+test_broken_lint()
+another_test()
